129755917dbd7a77b767faf678d074069284b0f7
Nijoane <nicajoane@outlook.com>
2021-10-30 22:53:45 -0300

    adding tableView and constrains

---
+++ StarWarsGuide/Models/Header/HomeTableViewHeader.swift
@@ -0,0 +1,18 @@
+//
+//  HomeTableViewHeader.swift
+//  StarWarsGuide
+//
+//  Created by Nicaely Joane on 28/10/21.
+//
+
+import UIKit
+
+class HomeTableViewHeader: UIView {
+ 
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var headerView: UIView!
+    
+    func confingView() {
+        headerView.backgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1)
+    }
+}
